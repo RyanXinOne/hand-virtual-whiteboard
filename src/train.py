f67f34ca79d0733e0cf56027de29787ded521b07
RyanXin <xyz@ryanxin.cn>
2023-01-02 12:28:29 +0000

    Update save

--- src/train.py
+++ src/train.py
@@ -55,11 +55,11 @@ def run():
     parser = argparse.ArgumentParser(description="Trains the YOLO model.")
     parser.add_argument("-m", "--model", type=str, default="config/yolov3-hagrid-3.cfg", help="Path to model definition file (.cfg)")
     parser.add_argument("-d", "--data", type=str, default="config/hagrid-3.data", help="Path to data config file (.data)")
-    parser.add_argument("-e", "--epochs", type=int, default=300, help="Number of epochs")
+    parser.add_argument("-e", "--epochs", type=int, default=30, help="Number of epochs")
     parser.add_argument("-v", "--verbose", action='store_true', help="Makes the training more verbose")
     parser.add_argument("--n_cpu", type=int, default=12, help="Number of cpu threads to use during batch generation")
-    parser.add_argument("--pretrained_weights", type=str, default="checkpoints/yolov3_ckpt_17_map0.70880.pth", help="Path to checkpoint file (.weights or .pth). Starts training from checkpoint model")
-    parser.add_argument("--pretrained_epochs", type=int, default=17, help="Number of epochs already trained. Used to determine the epoch number in the checkpoint filename.")
+    parser.add_argument("--pretrained_weights", type=str, default="checkpoints/yolov3_ckpt_25.pth", help="Path to checkpoint file (.weights or .pth). Starts training from checkpoint model")
+    parser.add_argument("--pretrained_epochs", type=int, default=25, help="Number of epochs already trained. Used to determine the epoch number in the checkpoint filename.")
     parser.add_argument("--checkpoint_interval", type=int, default=1, help="Interval of epochs between saving model weights")
     parser.add_argument("--evaluation_interval", type=int, default=1, help="Interval of epochs between evaluations on validation set")
     parser.add_argument("--multiscale_training", action="store_true", help="Allow multi-scale training")
@@ -209,16 +209,6 @@ def run():
 
             model.seen += imgs.size(0)
 
-        # #############
-        # Save progress
-        # #############
-
-        # Save model to checkpoint file
-        if epoch % args.checkpoint_interval == 0:
-            checkpoint_path = f"checkpoints/yolov3_ckpt_{epoch}.pth"
-            print(f"---- Saving checkpoint to: '{checkpoint_path}' ----")
-            torch.save(model.state_dict(), checkpoint_path)
-
         # ########
         # Evaluate
         # ########
@@ -245,6 +235,16 @@ def run():
                     ("validation/mAP", AP.mean()),
                     ("validation/f1", f1.mean())]
                 logger.list_of_scalars_summary(evaluation_metrics, epoch)
+
+        # #############
+        # Save progress
+        # #############
+
+        # Save model to checkpoint file
+        if epoch % args.checkpoint_interval == 0:
+            checkpoint_path = f"checkpoints/yolov3_ckpt_{epoch}_map{AP.mean():.5f}.pth"
+            print(f"---- Saving checkpoint to: '{checkpoint_path}' ----")
+            torch.save(model.state_dict(), checkpoint_path)
 
 
 if __name__ == "__main__":
