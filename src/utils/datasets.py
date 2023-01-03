5f0756e814a5df38a9cd4cdb0433540370d9cd0d
RyanXin <xyz@ryanxin.cn>
2023-01-03 21:31:13 +0000

    Adjust parameters

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -128,8 +128,7 @@ class ListDataset(Dataset):
 
         # Selects new image size every tenth batch
         if self.multiscale and self.batch_count % 10 == 0:
-            self.img_size = random.choice(
-                range(self.min_size, self.max_size + 1, 32))
+            self.img_size = random.choice(range(self.min_size, self.max_size + 1, 32))
 
         # Resize images to input shape
         imgs = torch.stack([resize(img, self.img_size) for img in imgs])
